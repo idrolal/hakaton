3608683cb51dba7d40258148d265d1f74b99dc8a
Irina Kasatkina <opacopa@inbox.ru>
2022-12-17 15:01:33 +0300

    Profile init

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
+import ProfilePage from './components/Profile/Profile';
 import { REQUEST_GET_ALL_USER } from './store/user/user.type';
 
 function App() {
@@ -7,22 +9,12 @@ function App() {
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch({type: REQUEST_GET_ALL_USER})
-  }, [])
+  }, [dispatch])
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/user/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProfilePage() {
+
+
+
+	return (
+		<div>
+			<h1>Личный кабинет</h1>
+			<div>Поменять имя пользователя</div>
+			<div>Посмотреть своих персонажей</div>
+			<div><h1>Начать игру</h1></div>
+		</div>
+	);
+}
+
+export default ProfilePage;
