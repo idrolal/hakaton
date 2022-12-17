2e449f31bd03c3ddee04bc37f340c9a06126bc93
Irina Kasatkina <opacopa@inbox.ru>
2022-12-17 16:22:55 +0300

    Edit profile links and input

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 function ProfilePage() {
+	const [edit, setEdit] = useState(false)
 
+	const handleEdit = () => {
+		setEdit(true)
+	}
+
+	const handleNameChange = () => {
+
+	}
 
 
 	return (
 		<div>
 			<h1>Личный кабинет</h1>
-			<div>Поменять имя пользователя</div>
-			<div>Посмотреть своих персонажей</div>
-			<div><h1>Начать игру</h1></div>
+			{edit ? (
+				<div>
+					<input type="text" onSubmit={handleNameChange} placeholder={'user.userName'} />
+				</div>
+			) : (
+				<div onClick={() => { setEdit(true) }} pointer >Поменять имя пользователя</div>
+			)}
+			<Link to='/user/game' ><h2>Посмотреть персонажей</h2></Link>
+			<Link to={`/game/${'user.id'}`}><h1>Начать игру</h1></Link>
 		</div>
 	);
 }
