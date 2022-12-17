9a042ffd09af5638b90a1f0107dfcc96a5dbcfe0
Anna <tima2396@yandex.ru>
2022-12-17 22:05:24 +0300

    ChoiceCharacterDone

--- client/src/components/СhoiceCharacter/ChoiceCharacter.jsx
+++ client/src/components/СhoiceCharacter/ChoiceCharacter.jsx
@@ -7,7 +7,7 @@ import { REQUEST_GET_ALL_CHARACTER } from "../../store/characters/character.type
 function ChoiceCharacter() {
 const dispatch = useDispatch();
 const { characters } = useSelector((state) => state.characters)
-// console.log(characters);
+console.log(characters);
   useEffect(()=>{
     dispatch({type: REQUEST_GET_ALL_CHARACTER})
   }, [])
@@ -16,9 +16,14 @@ const { characters } = useSelector((state) => state.characters)
     <div>
       <h2>Выбор персонажа</h2>
       {characters.map((character) => (
-         <div>
-            <img src={character.image} alt='character' />
-            
+         <div key={character.id}>
+            <img src={character['Role.image']} alt='character' />
+            <p>{character['Role.name']}</p>
+            <p>{character['Role.description']}</p>
+            <p>{character['Role.health']}</p>
+           <p>{character['Role.power']}</p>
+           <p>{character['Role.speed']}</p>
+           <button type="button">Выбрать</button>
          </div>
       )
       )}
